5d4a3a899b503e8f531dfa5677c785ea56bfe1b2
SangMino <sangmino1712@gmail.com>
2023-04-29 21:26:16 +0700

    remove public/css folder

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars").engine;
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// HTTP logger
+app.use(morgan("dev"));
+
+//template engine
+app.engine(
+  "hbs",
+  handlebars({
+    extname: ".hbs",
+  })
+);
+// app.engine('handlebars', engine());
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
