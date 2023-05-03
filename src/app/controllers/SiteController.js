752437814c91ec6487d13f8997857f97c60b69cf
SangMino <sangmino1712@gmail.com>
2023-05-03 16:57:04 +0700

    routes&controllers2

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,10 +1,23 @@
-class SiteController {
-  index(req, res) {
-    res.render("home");
+// Func constructor
+const SiteController =  {
+    index: (req, res) => {
+      res.render("home");
+    },
+    search: (req, res) => {
+      res.render("search");
+    }
   }
-  search(req, res) {
-    res.render("search");
-  }
-}
+  
+  module.exports = SiteController
+  
+// class constructor
+// class SiteController {
+//   index(req, res) {
+//     res.render("home");
+//   }
+//   search(req, res) {
+//     res.render("search");
+//   }
+// }
 
-module.exports = new SiteController
+// module.exports = new SiteController
